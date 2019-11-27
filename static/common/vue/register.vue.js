8fda2d9a2c19e1a8944ca1f79a234504f66c73e9
xuhaowei <1174061810@qq.com>
2019-11-27 16:23:22 +0800

    ...

--- static/common/vue/register.vue.js
+++
@@ -1,7 +0,0 @@
-export default {
-  resigterVue: () => {
-    `<script type="module">
-      import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
-    </script>`
-  }
-}
